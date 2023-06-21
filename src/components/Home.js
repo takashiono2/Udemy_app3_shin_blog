0de4bfe2df0009f701ad3b9c0adc65c65e7c5812
takashiono2 <takashiono710@gmail.com>
2023-06-22 02:04:14 +0900

    section4_43

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,42 @@
-import React from 'react';
+import React,{ useEffect, useState } from 'react';
+import "./Home.css";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase"
 
 const Home = () => {
+
+  const[postList, setPostList] = useState([]);
+  useEffect(()=>{
+    const getPosts = async() => {
+      const data = await getDocs(collection(db, "posts"));
+      setPostList(data.docs.map((doc)=>({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  },[]);
+
   return (
-    <div>Home</div>
+    <>
+    {postList.map((post) => {
+      return(
+        <div className="homePage" key={post.id}>
+          <div className="postContents">
+            <div className="postHeader">
+              <h1>{post.title}</h1>
+            </div>
+            <div className="postTextContainer">{ post.postText }</div>
+            <div className="nameAndDeleteButton">
+              <h3>@{ post.author.username }</h3>
+              <button>削除</button>
+            </div>
+          </div>
+        </div>
+      )
+    })
+    };
+    </>
   )
 }
 
 export default Home;
+
+
